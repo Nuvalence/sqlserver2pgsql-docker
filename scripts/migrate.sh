a002ea5ba8f0eb4637563b597c4f6d30a89c844b
dkiss5 <dkiss5@ford.com>
2022-04-18 14:07:55 -0400

    updated migrate.sh

--- scripts/migrate.sh
+++ scripts/migrate.sh
@@ -11,15 +11,17 @@ echo !!! Creating Kettle job && \
 echo !!! Executing before.sql && \
 # restricting access to key file as per psql requirements:
 chmod 0600 conf/client-key.pem && \
-PGPASSWORD=$DST_PWD psql -h $DST_HOST -p $DST_PORT -U $DST_USER -d $DST_DB -v sslmode=verify-ca -v sslrootcert=conf/server-ca.pem -v sslcert=conf/client-cert.pem -v sslkey=conf/client-key.pem -f before.sql
+PGPASSWORD=$DST_PWD psql -h $DST_HOST -p $DST_PORT -U $DST_USER -d $DST_DB {{#if (eval targetSslCerts '==' true)}}-v sslmode=verify-ca -v sslrootcert=conf/server-ca.pem -v sslcert=conf/client-cert.pem -v sslkey=conf/client-key.pem{{/if}} -f before.sql
 
+{{#if (eval postgresOnGcp '==' true)}}
 # removing SQL code that was causing issue in GCP due to lack of superuser permissions (https://github.com/dalibo/sqlserver2pgsql/issues/124):
 sed -i 's/DROP CAST IF EXISTS &#x28;varchar as date&#x29;//g' kettlejobs/migration.kjb
 sed -i 's/CREATE CAST &#x28;varchar as date&#x29; with inout as implicit;//g' kettlejobs/migration.kjb
 sed -i 's/DROP CAST &#x28;varchar as date&#x29;//g' kettlejobs/migration.kjb
+{{/if}}
 
 echo !!! Running Kettle job && \
 data-integration/kitchen.sh -file=kettlejobs/migration.kjb -level=rowlevel
 
 echo !!! Executing after.sql && \
-PGPASSWORD=$DST_PWD psql -h $DST_HOST -p $DST_PORT -U $DST_USER -d $DST_DB -v sslmode=verify-ca -v sslrootcert=conf/server-ca.pem -v sslcert=conf/client-cert.pem -v sslkey=conf/client-key.pem -f after.sql
+PGPASSWORD=$DST_PWD psql -h $DST_HOST -p $DST_PORT -U $DST_USER -d $DST_DB {{#if (eval targetSslCerts '==' true)}}-v sslmode=verify-ca -v sslrootcert=conf/server-ca.pem -v sslcert=conf/client-cert.pem -v sslkey=conf/client-key.pem{{/if}} -f after.sql
